3be7643ce1efec580bf3a42c0085cfd107e94573
D3N2-A <anmolsharma999937@gmail.com>
2022-05-31 09:28:46 +0530

    sounds and basic func

---
+++ index.js
@@ -0,0 +1,8 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSeq() {
+  var randomNumber = Math.floor(Math.random * 3 + 1);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
